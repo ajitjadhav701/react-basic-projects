111d5718e4a4f6d89b9d700b3a42551cda6e22ff
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-01 11:06:52 +0530

    fetching tours data and store in state

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      //console.log(response)
+      const tours = await response.json(); //convert response in json format
+      console.log(tours);
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        {" "}
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;
+
+/**
+ * 
+ * What is await used for?
+The await operator is used to wait for a Promise . 
+It can only be used inside an async function within regular JavaScript code;
+ however it can be used on its own with JavaScript modules.
+ */
