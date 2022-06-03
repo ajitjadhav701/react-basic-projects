8a6f4573ec9a93a9ba9cada8089eb4f9a1f3a79a
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-03 12:13:59 +0530

    done with structure ,color values amd error

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,33 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const[color,setColor]=useState();
+  const [error,setError]=useState(false);
+  const[list,setList]=useState([]);
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    try{
+      let colors=new Values(color).all(10);
+      console.log(colors)
+      
+    }catch (error){
+      setError(true);
+      console.log(error);
+    }
+  }
+  return <> 
+            <section className='container'>
+              <h3>color generator</h3>
+              <form action="" onSubmit={handleSubmit}>
+                <input className={`${error ? 'error':null}`} type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder='#f15025'/>
+                <button type='submit' className='btn'>submit</button>
+              </form>
+            </section>
+            <section className='colors'>
+                <h4>list goes here</h4>
+            </section>
+  </>
 }
 
 export default App
