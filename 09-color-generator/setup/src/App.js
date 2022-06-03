68cee74c6de293878bcbb86e411980b6b1ec995a
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-03 13:23:23 +0530

    done with copy the color to clipboard functionality

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,40 +1,57 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  const[color,setColor]=useState();
-  const [error,setError]=useState(false);
-  const[list,setList]=useState([]);
+  const [color, setColor] = useState();
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15025").all(5));
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    try{
-      let colors=new Values(color).all(10);
+    try {
+      let colors = new Values(color).all(5);
       //console.log(colors)
-      setList(colors)
-      
-    }catch (error){
+      setList(colors);
+      setError(false);
+    } catch (error) {
       setError(true);
       console.log(error);
     }
-  }
-  return <> 
-            <section className='container'>
-              <h3>color generator</h3>
-              <form action="" onSubmit={handleSubmit}>
-                <input className={`${error ? 'error':null}`} type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder='#f15025'/>
-                <button type='submit' className='btn'>submit</button>
-              </form>
-            </section>
-            <section className='colors'>
-               {list.map((color,index)=>{
-                 console.log(color)
-                 return <SingleColor key={index}{...color} index={index}hexColor={color.hex}/>
-               })}
-            </section>
-  </>
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            className={`${error ? "error" : null}`}
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button type="submit" className="btn">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color);
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,17 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = ({rgb,weight,index,hexColor}) => {
-  const [alert,setAlert]=useState(false);
-  const bcg=rgb.join(',');
-  console.log(hexColor)
-  const hex=rgbToHex(...rgb);
-  const hexValue=`#${hexColor}`
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  console.log(hexColor);
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
   //console.log(bcg);
-  return <article className={`color ${index > 8 &&'color-light'}`} style={{backgroundColor:`rgb(${bcg })`}}>
+
+  useEffect(()=>{
+   const timeout= setTimeout(() => {
+        setAlert(false);
+    }, 2000);
+    return ()=>clearTimeout(timeout)
+  },[alert])
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={()=>{
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue)
+      }}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
-  </article>
-}
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
+
+
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText
