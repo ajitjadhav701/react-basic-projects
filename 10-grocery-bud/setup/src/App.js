62c4113a3d5fa1de9712478c5fe2c5cd366445c6
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-06 13:52:57 +0530

    done grocery buds with localstorage setting list

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage=()=>{
+  let list=localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'));
+  }else return []
+}
 function App() {
   const [name,setName]=useState('');
-  const [list,setList]=useState([]);
+  const [list,setList]=useState(getLocalStorage());
   const[isEditing,setIsEditing]=useState(false);
   const[editId,setEditId]=useState(null);
   const[alert,setAlert]=useState({show:false,msg:'',type:''});
@@ -56,6 +62,9 @@ function App() {
     setEditId(id);
     setName(sepecifiItem.title)
   }
+  useEffect(()=>{
+    localStorage.setItem('list',JSON.stringify(list));
+  },[list])
   return <section className='section-center'>
   <form onSubmit={handleSubmit} className="grocery-form">
       {alert.show && <Alert {...alert}  removeAlert={showAlert} list={list}/>}
