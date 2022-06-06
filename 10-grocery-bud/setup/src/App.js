e22d3efceaad0f7bf9cc17f40d1a8678bc789418
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-06 12:36:27 +0530

    doen with grocery bud structure and add items

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,46 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name,setName]=useState('');
+  const [list,setList]=useState([]);
+  const[isEditing,setIsEditing]=useState(false);
+  const[editId,setEditId]=useState(null);
+  const[alert,setAletrt]=useState({show:false,msg:'',type:''});
+  const handleSubmit=(e)=>{
+      e.preventDefault();
+      if(! name){   //if empty text
+        //display alert
+        console.log('show alert')
+      }
+      else if(name && isEditing){
+        //deal with edit
+         console.log('show editing')
+      }
+      else{
+        //add item to list and show alert
+        const newItem={id:new Date().getTime().toString(),title:name};
+        setList([...list,newItem]);
+        setName('');
+      }
+  }
+  return <section className='section-center'>
+  <form onSubmit={handleSubmit} className="grocery-form">
+      {alert.show && <Alert/>}
+      <h3>grocery bud</h3>
+      <div className="form-control">
+        <input type="text" className='grocery' placeholder='e.g.eggs' value={name} onChange={(e)=>setName(e.target.value)}/>
+        <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit':'submit'}
+        </button>
+      </div>
+  </form>
+  {list.length > 0 &&  <div className="grocery-container">
+          <List items={list}/>
+          <button className='clear-btn'>clear items</button>
+        </div>
+      }
+       
+  </section>
 }
 
 export default App
