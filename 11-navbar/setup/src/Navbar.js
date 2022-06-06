71e61ec4cee00ada072c410b340ec52a47e4a6e0
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-06 21:20:09 +0530

    done with sidebar show /hide

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,20 +4,36 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks,setShowLinks]=useState(false);
+  const linksContainerRef=useRef(null);
+  const linksRef=useRef(null);
+
+  useEffect(()=>{
+    const linksHeight=linksRef.current.getBoundingClientRect().height;
+    if(showLinks){
+      linksContainerRef.current.style.height=`${linksHeight}px`
+    }else{
+      linksContainerRef.current.style.height='0px'
+    }
+    console.log(linksHeight);
+  },[showLinks])
   return <nav>
     <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo image" />
-          <button className='nav-toggle'><FaBars/></button>
+          <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}><FaBars/></button>
         </div>
-        <div className="links-container show-container">
-            <ul className="links">
+       
+        
+          <div className='links-container'ref={linksContainerRef}>
+            <ul className="links" ref={linksRef}>
                {links.map((link)=>{
                  const {id,url,text}=link;
                  return <li key={id}><a href={url}>{text}</a></li>
                })}
             </ul>
         </div>
+     
         <ul className="social-icons">
              {social.map((social)=>{
                  const {id,url,icon}=social;
