cae2d7d65bad5ce3c30bb12df38b9062b87601d6
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-06 20:37:21 +0530

    navbar basic structure and with data maping links

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,28 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return <nav>
+    <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo image" />
+          <button className='nav-toggle'><FaBars/></button>
+        </div>
+        <div className="links-container show-container">
+            <ul className="links">
+               {links.map((link)=>{
+                 const {id,url,text}=link;
+                 return <li key={id}><a href={url}>{text}</a></li>
+               })}
+            </ul>
+        </div>
+        <ul className="social-icons">
+             {social.map((social)=>{
+                 const {id,url,icon}=social;
+                 return <li key={id}><a href={url}>{icon}</a></li>
+               })}
+        </ul>
+    </div>
+  </nav>
 }
 
 export default Navbar
