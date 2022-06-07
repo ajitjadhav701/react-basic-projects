3d4a24f23e040b892de3023465c5368709e60b7e
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-07 14:27:10 +0530

    setting up context for stripe.

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
+  const{}=useGlobalContext();
   return <h2>hero component</h2>
 }
 
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,38 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+export const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setISubmenuOpen] = useState(true);
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setISubmenuOpen(true);
+  };
+  const closeModal = () => {
+    setISubmenuOpen(false);
+  };
+  return;
+  <AppContext.Provider
+    value={{
+      isSubmenuOpen,
+      isSidebarOpen,
+      openSubmenu,
+      openSidebar,
+      closeSubmenu,
+      closeSidebar,
+    }}
+  >
+    {children}
+  </AppContext.Provider>;
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
