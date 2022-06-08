dcafb623a555a74127b62e9e50bb90a433f6388f
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-08 20:42:32 +0530

    done functionality of increase /decrease amount in cart

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -8,6 +8,27 @@ const reducer=(state,action)=>{
         const newCart=state.cart.filter((item)=>item.id !==id)
         return {...state,cart:newCart}
     }
+    if(action.type==='INCREASE'){  
+      let tempCart=state.cart.map((item)=>{
+          if(item.id===action.payload){
+              return {...item,amount:item.amount+1}
+          }
+          return item;
+      });
+
+        return {...state,cart:tempCart}
+    }
+    if(action.type==='DECREASE'){  
+      let tempCart=state.cart.map((item)=>{
+          if(item.id===action.payload){
+              return {...item,amount:item.amount-1}
+          }
+          return item;
+      }).filter((item)=>item.amount !==0 );//here we did method chaining ..imp part
+
+        return {...state,cart:tempCart}
+    }
+ 
     return state;
 }
 
