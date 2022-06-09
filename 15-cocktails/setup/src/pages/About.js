88f1910cd167471331886686c6aa51a771f413e8
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-09 11:21:38 +0530

    update about page  and error page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente harum sit nulla similique nesciunt enim, praesentium eaque inventore quasi deserunt libero vel reiciendis odit voluptate dolores pariatur soluta, expedita, atque dolor! Odit excepturi illum placeat necessitatibus vitae corrupti mollitia aspernatur? Cupiditate architecto recusandae quas perferendis aperiam excepturi veritatis mollitia ea.</p>
+    </section>
   )
 }
 
