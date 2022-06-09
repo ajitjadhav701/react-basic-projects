be183abe97d2f40db105113cf3d3e5449d860141
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-09 12:23:31 +0530

    fetch items and display on home page

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -6,8 +6,35 @@ const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
   const [loading,setLoading]=useState(true);
-  const [searchTerm,setSearchTerm]=useState('a');
+  const [searchTerm,setSearchTerm]=useState('');
   const [cocktails,setCocktails]=useState([]);
+
+  console.log(cocktails)
+  const fetchDrinks =async()=>{
+    setLoading(true)
+    try{
+      const response=await fetch(`${url}${searchTerm}`);
+      const data =await response.json();
+      //console.log(data);
+      const {drinks}=data;
+      if(drinks){
+        const newCocktails=drinks.map((item)=>{
+          const{idDrink,strDrink,strDrinkThumb,strAlcoholic,strGlass}=item;
+          return {id:idDrink,name:strDrink,image:strDrinkThumb,info:strAlcoholic,glass:strGlass}
+        })
+        setCocktails(newCocktails)
+      }else{
+        setCocktails([]);
+      }
+      setLoading(false);
+    }catch(error){
+      console.log(error);
+      setLoading(false);
+    }
+  }
+  useEffect(()=>{
+    fetchDrinks();
+  },[searchTerm])
   return <AppContext.Provider value={{loading,searchTerm,cocktails,setSearchTerm}}>{children}</AppContext.Provider>
 }
 // make sure use
