613b2cff895d64498df8da95964b744292859885
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-10 10:47:15 +0530

    setup pagination logic and display single page

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -3,6 +3,15 @@ import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
   const {loading,data}=useFetch();
+  const [page,setPage]=useState(0);
+  const [followers,setFollowers]=useState([]);
+
+  useEffect(()=>{
+    if(loading){
+      return
+    }
+    setFollowers(data[page]);
+  },[loading])
   return <main>
       <div  className='section-title'>
         <h1>{loading ? 'loading..!!' : 'pagination'}</h1>
@@ -10,7 +19,7 @@ function App() {
       </div>
       <section className="followers">
         <div className="container"> 
-          {data.map((follower)=>{
+          {followers.map((follower)=>{
             return <Follower key={follower.id} {...follower}/>
           })}
         </div>
--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage=10;//on one page how many items 
+    const pages=Math.ceil(followers.length / itemsPerPage);//calculate how many pages will create
+    //console.log(pages);
+    //Array.from(length of array, data with map like function)
+    const newFollowers=Array.from({length:pages},(_,index)=>{
+        const start=index * itemsPerPage;//start value increment every iteration
+        return followers.slice(start,start + itemsPerPage)//starts with zero and end with 9
+    })
+    console.log(newFollowers);
+    return newFollowers;
+}
 
 export default paginate
