fcfb9ea4aa0caf4dc56a974ec1fd041f6c585da3
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-14 11:18:25 +0530

    fetch questions with temporary url and store in state

--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -24,6 +24,30 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const fetchQuestions=async(url)=>{
+    setLoading(true);
+    setWaiting(false);
+    const response= await axios.get(url).catch(error=>console.log(error))
+    //console.log(response)
+   if(response){
+    const data=response.data.results
+    //console.log(data)
+    if(data.length > 0){
+      setQuestions(data);
+      setLoading(false);
+      setWaiting(false);
+      setError(false);
+    }
+   }
+   else{
+    setWaiting(true)
+   }
+  }
+
+  useEffect(()=>{
+    fetchQuestions(tempUrl);
+  },[])
+
   return (
     <AppContext.Provider
       value={{
